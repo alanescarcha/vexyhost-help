ffc4ebd4a50b3246e666ba71819540c538922683
unknown <alanescarcha11@gmail.com>
2023-10-23 20:25:29 -0300

    first commit

---
+++ theme.config.jsx
@@ -0,0 +1,12 @@
+export default {
+    logo: (
+        <img width='70%' src="https://cdn.vexyhost.com/logo-white.svg" alt="VexyHost logo" />
+    ),
+    chat: {
+        link: 'https://discord.vexyhost.com/'
+    },
+    footer: {
+        component: null
+    }
+    // ... other theme options
+}
