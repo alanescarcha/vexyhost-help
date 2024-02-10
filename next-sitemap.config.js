94872665b8b1cd2d1595466ed0ab3de4c62d55ca
alanescarcha <alanescarcha11@gmail.com>
2024-02-10 06:43:32 -0300

    fixed sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://help.vexyhost.com',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
+    generateRobotsTxt: false,
+    outDir: './public',
+    transform: async (config, path) => {
+        let loc = path;
+        // index
+        if (path == '/index.en') {
+            loc = '/'
+        } else if (path == '/index.es') {
+            loc = 'es'
+        } else {
+            // others docs
+            if (path.endsWith('.en')) {
+                loc = path.substring(1, path.length - 3);
+            } else if (path.endsWith('.es')) {
+                loc = "es/" + path.substring(1, path.length - 3);
+            }
+        }
+        return {
+            loc: loc, // => this will be exported as http(s)://<config.siteUrl>/<path>
+            changefreq: config.changefreq,
+            priority: config.priority,
+            lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+            alternateRefs: config.alternateRefs ?? [],
+        }
+    },
 }
