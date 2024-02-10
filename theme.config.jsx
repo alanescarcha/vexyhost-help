06ba59e334309b01d608ed8c489721c251cb6eea
alanescarcha <alanescarcha11@gmail.com>
2024-02-10 06:08:37 -0300

    new head tags

--- theme.config.jsx
+++ theme.config.jsx
@@ -36,8 +36,13 @@ export default {
     },
     head: (
         <>
+            <meta name="keywords" content="vexyhost, vexy host, help, docs, knowledgebase, documentation, documentacion, guias, articulos, tutoriales"></meta>
             <meta name="description" content="😀 Explore our guides and tutorials to better understand your VexyHost services. 🙌" />
-            <link rel="shortcut icon" href="https://vexyhost.com/favicon.ico"></link>
+            <meta name="generator" content="Nextra" />
+            <link rel="alternate" href="https://help.vexyhost.com/" hreflang="en" />
+            <link rel="alternate" href="https://help.vexyhost.com/es" hreflang="es" />
+            <link rel="sitemap" href="/sitemap.xml" />
+            <link rel="shortcut icon" href="https://vexyhost.com/favicon.ico" />
         </>
     ),
     useNextSeoProps() {
